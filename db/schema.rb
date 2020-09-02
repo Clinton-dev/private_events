196bcd54b77eb1555424f1cd57ff260ebb478576
clinton wambugu <clintonwambugu@gmail.com>
2020-09-02 14:28:33 +0300

    Generate and migrate events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_162909) do
+ActiveRecord::Schema.define(version: 2020_09_02_112349) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "description"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
