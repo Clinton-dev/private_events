5602aabc896a0c7488759de643806360b0b91d87
clinton wambugu <clintonwambugu@gmail.com>
2020-09-04 23:58:07 +0300

    Modify User page to show list of attending events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
   has_many :attending1s, foreign_key: "attendee_id"
   has_many :events, through: :attending1s
   validates :name, presence: true
