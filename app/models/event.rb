992f3971b47b162a32c2a491ecc3dec06f839965
clinton wambugu <clintonwambugu@gmail.com>
2020-09-05 21:09:05 +0300

    Made past and upcoming methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :attending1s
   has_many :attendees, through: :attending1s, source: :attendee
   belongs_to :creator, class_name: "User"
+
+  def self.past(edate)
+    where('date <?', edate) if edate.present?
+  end
+
+  def self.upcoming(edate)
+    where('date >?', edate) if edate.present?
+  end
 end
